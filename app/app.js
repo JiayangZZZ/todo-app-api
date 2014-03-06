f8a863ead08f439ca73d5377e1797ef08721df19
Jiayang Zhang <jiayangzhang1104@gmail.com>
2014-03-06 16:21:07 +0800

    WIP

--- app/app.js
+++ app/app.js
@@ -1,14 +1,24 @@
 
 var express = require('express')
   , http = require('http')
-  , requirejs = require('requirejs');
+  , requirejs = require('requirejs')
+  , mysql = require('mysql')
+  , app = express();
 
-var app = express();
+// var connectionpool = mysql.createPool({
+//   host : 'localhost',
+//   user : 'root',
+//   password : '123',
+//   database : 'todo-app'
+// });
 
 app.configure(function() {
   app.set('port', process.env.PORT || 3000);
   app.use(express.logger('dev'));
   app.use(express.static(__dirname));
+  app.use(express.query());
+  app.use(express.bodyParser());
+  app.use(app.router);
 });
 
 requirejs.config({
@@ -17,8 +27,20 @@ requirejs.config({
 });
 
 
-app.get('/', function(req, res) {
-  res.send('hello world')
+// app.get('/', function(req, res) {
+//   res.send('hello world')
+// });
+// app.get('/', function(req, res) {});
+// app.put('/', function(req, res) {});
+// app.post('/', function(req, res) {});
+// app.delete('/', function(req, res) {});
+
+app.post('/oauth2/token', function(req, res) {
+  var username = req.param('username');
+  var password = req.param('password');
+  res.json({
+    token : 'jnkjhuihu'
+  })
 });
 
 http.createServer(app).listen(app.get('port'), function() {
